87ba8a4fc03f1b8cde6b07fb4824e9fd9d8d1f95
Sal Espinosa <sespinos@gmail.com>
2017-11-26 14:36:03 -0600

    Add shortcut to add alias

--- lib/shyness/cli.rb
+++ lib/shyness/cli.rb
@@ -3,9 +3,15 @@ require 'shyness'
 
 module Shyness
   class CLI < Thor
-    desc "ask", "Posts a question to the Ask application"
+    desc "ask QUESTION", "Posts a question to the Ask application"
     def ask(question)
       Shyness::Ask.add(question)
+      puts question
     end
+  end
+
+  desc "add_alias", "Adds an alias `ask` to the current user's .bash_profile so the user can use `ask` in place of `shyness ask`"
+  def add_alias
+    `echo 'alias ask="shyness ask"' >> ~/.bash_profile`
   end
 end
